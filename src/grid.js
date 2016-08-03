163100f400add78fc6a1e7ebace9b7d03039582c
gabriel montagné láscaris-comneno <gabriel@rojored.com>
2016-08-03 10:43:38 +0000

    stub setup grid template component.

--- src/grid.js
+++ src/grid.js
@@ -1,6 +1,10 @@
+import { rebind } from '@zambezi/d3-utils'
+import { createSetupGridTemplate } from './setup-grid-template'
+
 export function createGrid() {
   function grid(s) {
-    s.text('this is the grid')
+    console.log('d3Utils', rebind)
+    s.text('this is the grid+').call(createSetupGridTemplate())
   }
 
   return grid
---
+++ src/setup-grid-template.js
@@ -0,0 +1,13 @@
+export function createSetupGridTemplate() {
+
+  function setupGridTemplate(s) {
+    s.html('<h2>grid template</h2>')
+  }
+
+  setupGridTemplate.template = function(value) {
+    if (!arguments.length) return template
+  }
+
+  return setupGridTemplate
+
+}
