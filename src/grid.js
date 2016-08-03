57cad8c74c2fa9151e1c4891b42a1a39dc2a79f6
Gabriel Montagné Láscaris-Comneno <gabriel@rojored.com>
2016-08-03 13:42:12 +0100

    make sure id is set on component root.

--- src/grid.js
+++ src/grid.js
@@ -1,11 +1,15 @@
-import { rebind, call } from '@zambezi/d3-utils'
+import { rebind, call, each } from '@zambezi/d3-utils'
 import { createSetupGridTemplate } from './setup-grid-template'
+import { ensureId } from './ensure-id'
 import { compose } from 'underscore'
 
 export function createGrid() {
 
   const setupTemplate = createSetupGridTemplate()
-      , grid = compose(call(setupTemplate))
+      , grid = compose(
+          call(setupTemplate)
+        , each(ensureId)
+        )
 
   return grid
 }
