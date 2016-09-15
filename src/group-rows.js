8eba30eeacb32dec434057f886a144b2ca68fe3b
Gabriel Montagné <Gabriel.Montagne@icap.com>
2016-09-15 16:51:08 +0100

    Display synthetics rows.

--- src/group-rows.js
+++ src/group-rows.js
@@ -1,11 +1,14 @@
 import { createGroupRowsLayout } from './group-rows-layout'
 import { rebind } from '@zambezi/d3-utils'
+import { select } from 'd3-selection'
 
 import './group-rows.css'
 
 export function createGroupRows() {
-  
+
   const layout = createGroupRowsLayout()
+
+  let cache = null
 
   function groupRows(s) {
     s.each(groupRowsEach)
@@ -14,6 +17,14 @@ export function createGroupRows() {
   return rebind().from(layout, 'groupings')(groupRows)
 
   function groupRowsEach(d, i) {
+
     console.debug('groupRowsEach', layout(d), this)
+
+    const target = select(this)
+              .on('data-dirty.group-rows', () => cache = null)
+
+    if (!cache) cache = layout(d.rows || d)
+    d.rows = cache
+
   }
 }
