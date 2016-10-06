6dced4f1537de4af9f94f9c20363918c8f0e1c56
Gabriel Montagné <Gabriel.Montagne@icap.com>
2016-10-06 14:46:29 +0100

    Use a master grid to calculate the number of chunks

--- src/stacked-grid.js
+++ src/stacked-grid.js
@@ -1,10 +1,14 @@
 import { select } from 'd3-selection'
+import { appendIfMissing } from '@zambezi/d3-utils'
 import { createGrid } from './grid'
 import './stacked-grid.css'
 
 export function createStackedGrid() {
-  const rowCount = 7
-      , gridPool = []
+  const gridPool = []
+      , masterGrid = createGrid()
+            .serverSideFilterAndSort(true)
+            .useAfterMeasure(s => s.each(sliceDataForSlaveGrids))
+      , appendMaster = appendIfMissing('div.grid-page.master-grid')
 
   function stackedGrid(s) {
     s.each(stackedGridEach)
@@ -13,6 +17,30 @@ export function createStackedGrid() {
   return stackedGrid
 
   function stackedGridEach(d, i) {
+    const target = select(this).classed('zambezi-stacked-grid', true)
+        , masterGridTarget = target.select(appendMaster).call(masterGrid)
+  }
+
+  function sliceDataForSlaveGrids(d) {
+    const { rowHeight, bodyBounds } = d
+        , rowsPerPage = Math.floor(bodyBounds.height / rowHeight)
+        , chunks = d.reduce(toChunks, [])
+
+    debugger
+
+    function toChunks(acc, next, i) {
+      const chunkIndex = Math.floor(i / rowsPerPage)
+        , modIndex = i % rowsPerPage
+        , chunk = acc[chunkIndex] || []
+
+      chunk[modIndex] = next
+      acc[chunkIndex] = chunk
+
+      return acc
+    }
+  }
+
+  function stackedGridEachAfter(d, i) {
     const chunks = d.reduce(toChunks, [])
         , target = select(this).classed('zambezi-stacked-grid', true)
         , update =  target.selectAll('.grid-page')
@@ -32,7 +60,7 @@ export function createStackedGrid() {
       grid = createGrid().serverSideFilterAndSort(true)
       if (!i) grid.useAfterMeasure(s => s.each((d, i) => {
         console.log('Component called', d, i)
-        debugger
+
       }))
       gridPool[i] = grid
     }
@@ -44,14 +72,4 @@ export function createStackedGrid() {
         .call(grid)
   }
 
-  function toChunks(acc, next, i) {
-    const chunkIndex = Math.floor(i / rowCount)
-        , modIndex = i % rowCount
-        , chunk = acc[chunkIndex] || []
-
-    chunk[modIndex] = next
-    acc[chunkIndex] = chunk
-
-    return acc
-  }
 }
