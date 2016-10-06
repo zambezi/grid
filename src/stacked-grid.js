c3a27e3f949923b175ff7aa2aab5e20b33ffb808
Gabriel Montagné <Gabriel.Montagne@icap.com>
2016-10-06 11:39:35 +0100

    Stack grids with a grid pool.

--- src/stacked-grid.js
+++ src/stacked-grid.js
@@ -1,7 +1,9 @@
 import { select } from 'd3-selection'
+import { createGrid } from './grid'
 
 export function createStackedGrid() {
-  const rowCount = 40
+  const rowCount = 7
+      , gridPool = []
 
   function stackedGrid(s) {
     s.each(stackedGridEach)
@@ -17,9 +19,19 @@ export function createStackedGrid() {
         , enter = update.enter().append('div').classed('grid-page', true)
         , exit  = update.exit().remove()
 
-    update.merge(enter).text('hi, i am a grid page')
+    update.merge(enter).each(drawGridPage)
 
     console.log('\nChunks: ', chunks)
+  }
+
+  function drawGridPage(d, i) {
+    const grid = gridPool[i] || createGrid()
+    select(this)
+        .style('height', '300px')
+        .style('width', '500px')
+        .style('display', 'inline-block')
+        .call(grid)
+    gridPool[i] = grid
   }
 
   function toChunks(acc, next, i) {
