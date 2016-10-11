5f685fd187b3be581d44af22096432ea6a5243bd
Gabriel Montagné <Gabriel.Montagne@icap.com>
2016-10-11 15:26:15 +0100

    Attach handlers to expanders to make them work

--- src/simple-nested-row-expanders.js
+++ src/simple-nested-row-expanders.js
@@ -1,11 +1,10 @@
 import './simple-nested-row-expanders.css'
 import { select } from 'd3-selection'
+import { unwrap } from './unwrap-row'
 
 export function createSimpleNestedRowExpanders() {
 
   function simpleNestedRowExpanders(d) {
-    console.log('Called on', this, d)
-
     const row = d.row
         , hasNested = !!(row.children && row.children.length)
         , isExpand = hasNested && row.expanded
@@ -14,6 +13,28 @@ export function createSimpleNestedRowExpanders() {
     select(this).classed('simple-nested-expander-cell', true)
         .classed('is-expand', isExpand)
         .classed('is-collapse', isCollapse)
+        .on('click.simple-expander-toggle',
+          isExpand   ? collapse
+        : isCollapse ? expand
+        : null
+        )
+        .on('click.simple-expander-redraw', hasNested ? redraw : null)
+
+    function expand(d) {
+      unwrap(d.row).expanded = true
+    }
+
+    function collapse(d) {
+      unwrap(d.row).expanded = false
+    }
+
+    function redraw() {
+      event.stopImmediatePropagation()
+
+      select(this)
+        .dispatch('data-dirty', { bubbles: true })
+        .dispatch('redraw', { bubbles: true })
+    }
   }
 
   return simpleNestedRowExpanders
