9934b53f124d0cb0ee11e800401034fffb45868a
Gabriel Montagné <Gabriel.Montagne@icap.com>
2016-10-11 14:57:23 +0100

    Draw expanders with heavy Unicode characters

--- src/simple-nested-row-expanders.js
+++ src/simple-nested-row-expanders.js
@@ -1,7 +1,19 @@
+import './simple-nested-row-expanders.css'
+import { select } from 'd3-selection'
+
 export function createSimpleNestedRowExpanders() {
 
   function simpleNestedRowExpanders(d) {
     console.log('Called on', this, d)
+
+    const row = d.row
+        , hasNested = !!(row.children && row.children.length)
+        , isExpand = hasNested && row.expanded
+        , isCollapse = hasNested && !isExpand
+
+    select(this).classed('simple-nested-expander-cell', true)
+        .classed('is-expand', isExpand)
+        .classed('is-collapse', isCollapse)
   }
 
   return simpleNestedRowExpanders
